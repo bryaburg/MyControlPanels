60c3d36d6012f07cae5d5d6dc9eed10e301d3a29
eggersz <eggersz@amazon.com>
2021-04-26 03:31:11 -0500

    Revert "Machine Display"
    This reverts commit 2b2f1270707a7c6a0c62f8faca44f5c46c206210.

--- AdvancedHMI/MachineDisplay.vb
+++ AdvancedHMI/MachineDisplay.vb
@@ -1,7 +1,6 @@
 ﻿Public Class MachineDisplay
     Private Sub MachineDisplay_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.EthernetIPforCLXCom1.IPAddress = SlamControls.EthernetIPforCLXCom1.IPAddress
-        Prt1RodEx.Visible = False
     End Sub
 
     '//Handles Date and Time
@@ -14,17 +13,5 @@
 
     Private Sub MacDisExit_Click(sender As Object, e As EventArgs) Handles MacDisExit.Click
         Me.Close()
-    End Sub
-
-    Private Sub prtOnePE2_Click(sender As Object, e As EventArgs) Handles prtOnePE2.Click
-
-    End Sub
-
-    Private Sub Prt1RodEx_Click(sender As Object, e As EventArgs) Handles Prt1RodEx.Click
-
-    End Sub
-
-    Private Sub showSlams_Click(sender As Object, e As EventArgs) Handles showSlams.Click
-        SlamControls.Show()
     End Sub
 End Class
